edb978d286a74651d5b84482a75a2bcd4f660f21
Maks <mshevkun.dev@gmail.com>
2024-07-23 13:54:09 -0400

    Add CityType and test cases for city houses and government buildings

---
+++ my-app/src/02/02_02.ts
@@ -0,0 +1,28 @@
+export type StreetType = {
+  title: string;
+};
+
+export type AddressType = {
+  number?: number;
+  street: StreetType;
+};
+
+export type HouseType = {
+  buildedAt: number;
+  repaired: boolean;
+  address: AddressType;
+};
+
+export type GovernmentBuildingsType = {
+  type: string;
+  budget: number;
+  staffCount: number;
+  address: AddressType;
+};
+
+export type CityType = {
+  title: string;
+  houses: Array<HouseType>;
+  governmentBuildings: Array<GovernmentBuildingsType>;
+  citizensNumber: number;
+};
